25d72835e4ffa1ccca032f6573717d2f51931ab2
Yangkro <907661446@qq.com>
2021-10-16 23:29:13 +0800

    test

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
+//初始化
 ReactDOM.render(
   <React.StrictMode>
     <App />
