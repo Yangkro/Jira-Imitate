b39d628e1403da80658db203c398c2ca1e0eab69
Yangkro <907661446@qq.com>
2022-03-05 23:37:16 +0800

    feat:引入jira-dev-server

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { loadDevTools } from "jira-dev-tool";
 //初始化
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+loadDevTools(() =>
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  )
 );
 
 reportWebVitals();
